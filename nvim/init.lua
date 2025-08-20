e741b17b3b8b89da88efb6f2e5906bf4c852c5db
mdonaka <tmdonkt042+eg@gmail.com>
2025-08-21 02:04:24 +0900

    remove init.vim

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,9 +2,3 @@ require("config.options")
 require("config.filetypes")
 require("config.keymaps")
 require("config.lazy")
-
--- init.vimを移植完了まで読み込んでおく
-local legacy = vim.fn.stdpath("config") .. "/init_legacy.vim"
-if vim.fn.filereadable(legacy) == 1 then
-  vim.cmd("source " .. legacy)
-end
